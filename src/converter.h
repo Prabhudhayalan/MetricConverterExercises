385820957ef299b96443500daef3b719b110c5b9
Prabhudhayalan <prabhudhayalan96@gmail.com>
2020-03-31 10:15:17 +0200

    I have defined the converter file

--- src/converter.h
+++ src/converter.h
@@ -2,9 +2,9 @@
 #define CONVERTER_H
 
 /* Task 1: Add your #defines here */
-#define MILLIMETRESINCENTIMETRE 10
-#define CENTIMETRESINMETRE 100
-#define METRESINKILOMETRE 1000
+#define MILLIMETRES_IN_CENTIMETRE 10
+#define CENTIMETRES_IN_METRE 100
+#define METRES_IN_KILOMETRE 1000
 
 
 /* Task 2: Uncomment the following functions one by one and provide
