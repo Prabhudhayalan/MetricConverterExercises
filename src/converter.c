c579e0d3f5180bfc67562ee89a2d4b89abd4bf0e
Prabhudhayalan <prabhudhayalan96@gmail.com>
2020-03-31 11:11:15 +0200

    created the converter.c

---
+++ src/converter.c
@@ -0,0 +1,10 @@
+#include "converter.h"
+
+double centimetres_to_millimetres(double centimetres)
+{
+  double millimetres;
+
+  millimetres = centimetres*MILLIMETRES_IN_CENTIMETRE;
+
+  return millimetres;
+}
